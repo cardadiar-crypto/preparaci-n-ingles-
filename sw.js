f726563c3ffc30b739dc383caa4066e8f4c2c9a8
cardadiar <cardadiar@gmail.com>
2026-09-04 11:48:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'english-b2-v3';
+const CACHE = 'english-b2-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -32,14 +32,15 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        if (res && res.status === 200) {
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((res) => {
           const clone = res.clone();
           caches.open(CACHE).then((c) => c.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+          return res;
+        })
+        .catch(() => cached);
+    })
   );
 });
